3350161b84ebcbf68be3e023ab9b1cf0d6512b3a
JackSharpe94 <jack.sharpe233@gmail.com>
2022-09-30 15:36:52 +0100

    added todo buttons and components

--- to-do/src/app/app.module.ts
+++ to-do/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ToDoItemsComponent } from './to-do-items/to-do-items.component';
+import { ToDoItemComponent } from './to-do-item/to-do-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToDoItemsComponent,
+    ToDoItemComponent
   ],
   imports: [
     BrowserModule
